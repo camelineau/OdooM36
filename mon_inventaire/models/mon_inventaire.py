465bc076063b263cbfbdfe39d6f49bafce77d366
clemenceamelineau <104129924+clemenceamelineau@users.noreply.github.com>
2022-04-29 09:19:02 +0200

    Add files via upload

--- mon_inventaire/models/mon_inventaire.py
+++ mon_inventaire/models/mon_inventaire.py
@@ -11,12 +11,32 @@ class Materiel(models.Model):
     type = fields.Selection([('Switch', 'SWITCH'),('Routeur', 'ROUTEUR')])
     marque_id = fields.Many2one('mon_inventaire.marque', string='Marque')
     description = fields.Text('Liste du matériel')
+    responsable_id = fields.Many2one('mon_inventaire.responsable', string='Responsable')
+    responsable_id_full_name = fields.Char(related = 'responsable_id.full_name')
 
 class Marque(models.Model):
     _name = 'mon_inventaire.marque'
     _description = 'mon_inventaire.marque'
     
     name = fields.Char(string="Nom de la marque", required = True)
-    materiel_ids = fields.One2many('mon_inventaire.materiel', 'marque_id', string='Modèle associé')
+    materiel_ids = fields.One2many('mon_inventaire.materiel', 'marque_id', string='Matériel associé')
     description = fields.Text()
+    
+class Responsable(models.Model):
+    _name = 'mon_inventaire.responsable'
+    _description = 'mon_inventaire.responsable'
+    
+    name = fields.Char(string="Nom", required = True)
+    first_name = fields.Char(string="Prénom", required = True)
+    full_name = fields.Char(string = "Responsable", compute="_compute_name", store=True)
+    email = fields.Char(string="Email")
+    num = fields.Char(string="Numéro de téléphone")
+    materiel_ids = fields.One2many('mon_inventaire.materiel', 'responsable_id', string='Matériel')
+    materiel_ids_name = fields.Char(related = 'materiel_ids.name')
+    
+    @api.depends('name','first_name')
+    def _compute_name(self):
+        for rec in self:
+            if rec.name and rec.first_name:
+                rec.full_name = rec.name + ' ' + rec.first_name
     
